d249d07b045118c78c6e8d02b9fa3646e60ac97e
T kumagai <4956799+friendbear@users.noreply.com>
2024-08-15 15:31:41 +0900

    Simple 3 steps dag

---
+++ dags/depends_on_past.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 8, 15),
+    "schedule_cron": "1/60 * * * *",
+}
+
+def first_func():
+    print("This func is executed by the first task")
+
+def second_func():
+    print("This func is executed by the second task")
+    raise ValueError("This will turn the python task state to failed")
+
+def third_func():
+    print("This func is executed by the third task")
+
+with DAG(
+    "depends_on_past",
+    default_args=default_args,
+    schedule_interval=timedelta(1),
+    catchup=False,
+) as dag:
+    first_task = PythonOperator(
+        task_id="first_task",
+        python_callable=first_func,
+    )
+
+    second_task = PythonOperator(
+        task_id="second_task",
+        python_callable=second_func,
+        depends_on_past=True,
+    )
+
+    third_task = PythonOperator(
+        task_id="third_task",
+        python_callable=third_func,
+        depends_on_past=True,
+    )
+
+    first_task >> second_task >> third_task
